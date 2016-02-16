c678e55f3f49fbce6df2e11e8b69364acd65163a
cassdotcom <cassdotcom@users.noreply.github.com>
2016-02-16 20:05:21 +0000

    Update Create-AssetID.ps1
    removed project code

--- scripts/Create-AssetID.ps1
+++ scripts/Create-AssetID.ps1
@@ -1,18 +1,11 @@
 function Create-AssetID
 {
-    Param(
-        [System.String]
-        $projCode
-    )
-
     . "C:\Users\ac00418\Documents\WindowsPowerShell\FunctionLibrary\FunctionScripts\Create-Random.ps1"
 
-    $res = $projCode + "-"
-    $res += (Create-Random -wordLength 8) + "-"
+    $res = (Create-Random -wordLength 8) + "-"
     $res += (Create-Random -wordLength 4) + "-"
     $res += (Create-Random -wordLength 4) + "-"
-    $res += (Create-Random -wordLength 4) + "-"
-    $res += (Create-Random -wordLength 12) + "-00"
+    $res += (Create-Random -wordLength 8)
 
     Write-Output $res
 }
